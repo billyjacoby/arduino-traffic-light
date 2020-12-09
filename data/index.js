814ba62986f33b610044d73e03ca99f92370e9fa
Billy Jacoby <billyjacoby@eros.local>
2020-12-08 21:52:48 -0500

    wokring mostly

--- data/index.js
+++ data/index.js
@@ -1,9 +1,14 @@
-// console.log(JSON.stringify(patterns));
-
 const offOpacity = 0.2;
 const onOpacity = 1.0;
 
-function toggleLight(lightElement) {
+function requestData(url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {};
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function toggleLight(lightElement, lightColor) {
   let curOpacity = getComputedStyle(lightElement).getPropertyValue("opacity");
   let lightOn;
   if (curOpacity > 0.5) {
@@ -14,10 +19,69 @@ function toggleLight(lightElement) {
 
   if (lightOn) {
     lightElement.style.opacity = offOpacity;
+    if (lightColor === "red") {
+      redLightOff();
+    } else if (lightColor === "yellow") {
+      yellowLightOff();
+    } else if (lightColor === "green") {
+      greenLightOff();
+    }
   }
   if (!lightOn) {
     lightElement.style.opacity = onOpacity;
+    if (lightColor === "red") {
+      redLightOn();
+    } else if (lightColor === "yellow") {
+      yellowLightOn();
+    } else if (lightColor === "green") {
+      greenLightOn();
+    }
   }
+}
+
+// Red Light
+function redLightOn() {
+  let el = document.getElementsByClassName("red")[0];
+  el.style.opacity = onOpacity;
+  console.log("Red on");
+  requestData("/relay1/on");
+}
+
+function redLightOff() {
+  let el = document.getElementsByClassName("red")[0];
+  el.style.opacity = offOpacity;
+  console.log("Red off");
+  requestData("/relay1/off");
+}
+
+// Yellow Light
+function yellowLightOn() {
+  let el = document.getElementsByClassName("yellow")[0];
+  el.style.opacity = onOpacity;
+  console.log("yellow on");
+  requestData("/relay2/on");
+}
+
+function yellowLightOff() {
+  let el = document.getElementsByClassName("yellow")[0];
+  el.style.opacity = offOpacity;
+  console.log("yellow off");
+  requestData("/relay2/off");
+}
+
+// Green Light
+function greenLightOn() {
+  let el = document.getElementsByClassName("green")[0];
+  el.style.opacity = onOpacity;
+  console.log("green on");
+  requestData("/relay3/on");
+}
+
+function greenLightOff() {
+  let el = document.getElementsByClassName("green")[0];
+  el.style.opacity = offOpacity;
+  console.log("green off");
+  requestData("/relay3/off");
 }
 
 function turnLightOn(el) {
@@ -28,64 +92,10 @@ function turnLightOff(el) {
   el.style.opacity = offOpacity;
 }
 
-function executeStateCall(state, el) {
-  if (state) {
-    turnLightOn(el[0]);
-  } else {
-    turnLightOff(el[0]);
-  }
+function startPattern() {
+  requestData("/pattern/start");
 }
 
-function setLightState(patternItem) {
-  executeStateCall(
-    patternItem.state.red,
-    document.getElementsByClassName("red")
-  );
-  executeStateCall(
-    patternItem.state.yellow,
-    document.getElementsByClassName("yellow")
-  );
-  executeStateCall(
-    patternItem.state.green,
-    document.getElementsByClassName("green")
-  );
-  ``;
-}
-
-function sleep(delay) {
-  return new Promise((resolve) => setTimeout(resolve, delay));
-}
-
-let currentRunningPattern;
-
-function runPattern(patternSlug) {
-  let curPattern = patterns.filter((item) => item.slug === patternSlug)[0];
-  // if (curPattern.continuous) {
-  //   let totalDuration = curPattern.pattern.reduce((acc, cur) => acc + cur, 0);
-  //   // do forever
-  // } else {
-  //   // do once
-  // }
-  curPattern.pattern.forEach((item, index) => {
-    let timeout = item.duration;
-
-    if (index === 0) {
-      timeout = 0;
-    }
-
-    setTimeout(() => {
-      setLightState(item);
-    }, timeout);
-  });
-}
-
-function addEventListenerList(list, event, fn) {
-  for (var i = 0, len = list.length; i < len; i++) {
-    list[i].addEventListener(event, fn, false);
-  }
-}
-function recordPattern() {
-  addEventListenerList(document.getElementsByClassName("light"), "click", () =>
-    console.log("clicked: ", event.target)
-  );
+function stopPattern() {
+  requestData("/pattern/stop");
 }
--- traffic-light-controller.ino
+++ traffic-light-controller.ino
@@ -1,58 +1,30 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
+
 
 // Import required libraries
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <FS.h>
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
 
 // Replace with your network credentials
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "Jakes Traffic Light";
+const char* password = "password";
+
+IPAddress local_IP(10,1,1,1);
+IPAddress gateway(10,1,1,1);
+IPAddress subnet(255,255,255,0);
 
 // Set LED GPIO
 const int ledPin = 2;
 // Stores LED state
 String ledState;
 
+bool trafficLightPatternRunning = false;
+bool isComplete = false;
+int curMillis = millis();
+
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
-
-String getTemperature() {
-  float temperature = bme.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  //float temperature = 1.8 * bme.readTemperature() + 32;
-  Serial.println(temperature);
-  return String(temperature);
-}
-  
-String getHumidity() {
-  float humidity = bme.readHumidity();
-  Serial.println(humidity);
-  return String(humidity);
-}
-
-String getPressure() {
-  float pressure = bme.readPressure()/ 100.0F;
-  Serial.println(pressure);
-  return String(pressure);
-}
 
 // Replaces placeholder with LED state value
 String processor(const String& var){
@@ -66,40 +38,97 @@ String processor(const String& var){
     }
     Serial.print(ledState);
     return ledState;
-  }
-  else if (var == "TEMPERATURE"){
-    return getTemperature();
-  }
-  else if (var == "HUMIDITY"){
-    return getHumidity();
-  }
-  else if (var == "PRESSURE"){
-    return getPressure();
-  }  
+  } else if (var == "RELAY1"){
+  return "UNAVAIL";
+}
+else if (var == "RELAY2"){
+  return "UNAVAIL";
+}
+else if (var == "RELAY3"){
+  return "UNAVAIL";
+} 
+}
+
+void relayOneOn() {
+  digitalWrite(D5, LOW);
+}
+
+void relayOneOff() {
+  digitalWrite(D5, HIGH);
+}
+
+void relayTwoOn() {
+  digitalWrite(D6, LOW);
+}
+
+void relayTwoOff() {
+  digitalWrite(D6, HIGH);
+}
+
+void relayThreeOn() {
+  digitalWrite(D7, LOW);
+}
+
+void relayThreeOff() {
+  digitalWrite(D7, HIGH);
+}
+
+void startTrafficLight() {
+  trafficLightPatternRunning = true;
+  isComplete = true;
+}
+
+void stopTrafficLight() {
+  trafficLightPatternRunning = false;
+  relayOneOff();
+  relayTwoOff();
+  relayThreeOff();
+}
+
+void trafficLightPattern() {
+//  Serial.println(millis());
+//  Serial.println(curMillis);
+  if (isComplete) {
+      curMillis = millis();
+      isComplete = false;
+    }
+  if (curMillis + 1000 == millis()) {
+    Serial.println("change one");
+    relayOneOn();
+    relayTwoOff();
+    relayThreeOff();
+  } else if (curMillis + 6000 == millis()) {
+    Serial.println("change two");
+    relayOneOff();
+    relayTwoOn();
+    relayThreeOff();
+    } else if (curMillis + 11000 == millis()) {
+    Serial.println("change two");
+    relayOneOff();
+    relayTwoOff();
+    relayThreeOn();
+    
+    } else if (curMillis + 16000 == millis()) {
+      isComplete = true;
+    }
+  
 }
  
 void setup(){
   // Serial port for debugging purposes
+  Serial.print("Setting soft-AP configuration ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "IP Ready" : "IP Failed!");
+  Serial.println(WiFi.softAP(ssid) ? "AP Ready" : "AP Failed!");
   Serial.begin(115200);
   pinMode(ledPin, OUTPUT);
-
-  // Initialize the sensor
-  if (!bme.begin(0x76)) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
-  }
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
 
   // Initialize SPIFFS
   if(!SPIFFS.begin()){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
-  }
-
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
   }
 
   // Print ESP32 Local IP Address
@@ -115,6 +144,15 @@ void setup(){
     request->send(SPIFFS, "/style.css", "text/css");
   });
 
+  // Route to load index.js file
+  server.on("/index.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.js", "text/javascript");
+  });
+    // Route to load patterns.js file
+  server.on("/patterns.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/patterns.js", "text/javascript");
+  });
+
   // Route to set GPIO to HIGH
   server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
     digitalWrite(ledPin, HIGH);    
@@ -127,22 +165,55 @@ void setup(){
     request->send(SPIFFS, "/index.html", String(), false, processor);
   });
 
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", getTemperature().c_str());
+  // Relay One Routes
+  server.on("/relay1/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    relayOneOn();   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
   });
-  
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", getHumidity().c_str());
+    server.on("/relay1/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    relayOneOff();   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
   });
-  
-  server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", getPressure().c_str());
+
+    // Relay Two Routes
+  server.on("/relay2/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    relayTwoOn();   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
   });
+    server.on("/relay2/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    relayTwoOff();   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+    // Relay Three Routes
+  server.on("/relay3/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    relayThreeOn();   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+    server.on("/relay3/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    relayThreeOff();   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  // Pattern Routes
+  server.on("/pattern/start", HTTP_GET, [](AsyncWebServerRequest *request){
+    startTrafficLight();   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+  server.on("/pattern/stop", HTTP_GET, [](AsyncWebServerRequest *request){
+    stopTrafficLight();   
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
 
   // Start server
   server.begin();
 }
- 
-void loop(){
+
+void loop() {
   
+  if (trafficLightPatternRunning) {
+    trafficLightPattern();
+  }
 }
